9286681cbc45b469eb2f3cb27b7e13f567a70a28
Shantanu Kumar <kumar.shantanu@gmail.com>
2026-01-29 21:04:13 +0530

    Bump Clojars artifact version to released 0.1.0
    The Clojars artifacts are now updated to match the version released
    on Clojars.

--- apps/hello-mcp-clj/project-graal.clj
+++ apps/hello-mcp-clj/project-graal.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "https://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.12.4"]
-                 [io.github.plumce/plumcp.core-json-charred "0.1.0-SNAPSHOT"]
+                 [io.github.plumce/plumcp.core-json-charred "0.1.0"]
                  [com.github.clj-easy/graal-build-time "1.0.5"]]
   :main ^:skip-aot hello-mcp-clj.core
   :target-path "target/%s"
--- apps/hello-mcp-clj/project.clj
+++ apps/hello-mcp-clj/project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "https://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.12.4"]
-                 [io.github.plumce/plumcp.core-json-charred "0.1.0-SNAPSHOT"]]
+                 [io.github.plumce/plumcp.core-json-charred "0.1.0"]]
   :main ^:skip-aot hello-mcp-clj.core
   :target-path "target/%s"
   :profiles {:uberjar
