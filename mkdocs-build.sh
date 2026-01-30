1bd09345725051b8cb38bf52f1477179fc2054a2
Shantanu Kumar <kumar.shantanu@gmail.com>
2026-01-30 13:37:37 +0530

    Add user-id, group-id to Docker commands
    When user-id and group-id are not added to the Docker commands, any
    created file or directory is owned by 'root', which is a nuisance.
    We specify the user-id and group-id to avoid the issue.

--- mkdocs-build.sh
+++ mkdocs-build.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 #
-docker run --rm -it -v ${PWD}:/docs -p 8000:8000 squidfunk/mkdocs-material:9 build
-
+docker run --rm -it -v ${PWD}:/docs -u $(id -u):$(id -g) -p 8000:8000 squidfunk/mkdocs-material:9 build
--- mkdocs-serve.sh
+++ mkdocs-serve.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 #
 
-docker run --rm -it -v ${PWD}:/docs -p 8000:8000 squidfunk/mkdocs-material:9 serve --livereload --dev-addr 0.0.0.0:8000
-
+docker run --rm -it -v ${PWD}:/docs -u $(id -u):$(id -g) -p 8000:8000 squidfunk/mkdocs-material:9 serve --livereload --dev-addr 0.0.0.0:8000
